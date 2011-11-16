c7abc3cebe291c7d31cb62e8d3c0630d2ed3bdc6
Jeff Schornick <jeff@schornick.org>
2011-11-16 18:48:36 -0500

    Header comment for debug.php

--- debug.php
+++ debug.php
@@ -1,5 +1,11 @@
 <?
 
+# debug.php
+
+# Copyright (c) 2011 Jeffrey Schornick <jeff@schornick.org>
+# Licensed under The MIT License
+# http://www.opensource.org/licenses/mit-license.php
+
 # Debug output, formatted (or hidden) based on the $gDebug global
 # - 0 = no debugging, 1 = inline comment, 2 = visable, 3 = raw
 function debug( $msg='' ) {
